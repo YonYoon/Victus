d202dc920295a19207a9a60ef95b7efa98a50409
YonYoon <zhzhzh1806@gmail.com>
2024-01-08 22:08:21 +0500

    Removed boilerplate code and put generated diet

--- Victus/Views/CalendarView.swift
+++ Victus/Views/CalendarView.swift
@@ -15,130 +15,15 @@ struct CalendarView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                VStack(alignment: .leading) {
-                    Text("Понедельник")
-                        .font(.title)
-                        .fontWeight(.medium)
-                    
-                    HStack {
-                        Image("friedEggs")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 100, height: 70)
-                            .clipShape(RoundedRectangle(cornerRadius: 15))
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text("Яичница")
-                                .font(.title2)
-                                .fontWeight(.medium)
-                            Text("1000 тг")
-                                .foregroundStyle(.secondary)
-                                .fontWeight(.semibold)
-                        }
-                        .padding(.leading)
-                    }
-                    Divider()
-                    HStack {
-                        Image("grilledChicken")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 100, height: 70)
-                            .clipShape(RoundedRectangle(cornerRadius: 15))
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text("Курица с салатом")
-                                .font(.title2)
-                                .fontWeight(.medium)
-                            Text("1200 тг")
-                                .foregroundStyle(.secondary)
-                                .fontWeight(.semibold)
-                        }
-                        .padding(.leading)
-                    }
-                    Divider()
-                    HStack {
-                        Image("fish")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 100, height: 70)
-                            .clipShape(RoundedRectangle(cornerRadius: 15))
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text("Рыба с овощами")
-                                .font(.title2)
-                                .fontWeight(.medium)
-                            Text("1400 тг")
-                                .foregroundStyle(.secondary)
-                                .fontWeight(.semibold)
-                        }
-                        .padding(.leading)
-                    }
-                }
-                .padding()
-                
-                
-                VStack(alignment: .leading) {
-                    Text("Вторник")
-                        .font(.title)
-                        .fontWeight(.medium)
-                    
-                    HStack {
-                        Image("oatmeal")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 100, height: 70)
-                            .clipShape(RoundedRectangle(cornerRadius: 15))
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text("Каша")
-                                .font(.title2)
-                                .fontWeight(.medium)
-                            Text("800 тг")
-                                .foregroundStyle(.secondary)
-                                .fontWeight(.semibold)
-                        }
-                        .padding(.leading)
-                    }
-                    Divider()
-                    HStack {
-                        Image("lentilSoup")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 100, height: 70)
-                            .clipShape(RoundedRectangle(cornerRadius: 15))
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text("Чечевичный суп")
-                                .font(.title2)
-                                .fontWeight(.medium)
-                            Text("1100 тг")
-                                .foregroundStyle(.secondary)
-                                .fontWeight(.semibold)
-                        }
-                        .padding(.leading)
-                    }
-                    Divider()
-                    HStack {
-                        Image("beef")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 100, height: 70)
-                            .clipShape(RoundedRectangle(cornerRadius: 15))
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text("Говядина с брокколи")
-                                .font(.title2)
-                                .fontWeight(.medium)
-                            Text("1500 тг")
-                                .foregroundStyle(.secondary)
-                                .fontWeight(.semibold)
-                        }
-                        .padding(.leading)
-                    }
-                }
-                .padding()
+                Text(viewModel.chatMessages.filter( {$0.role == .assistant } ).last?.content ?? "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo leo, iaculis sed suscipit in, varius ut justo. Vivamus vel pretium nisi, eget dignissim libero. Maecenas porta vel ante nec tristique. Ut ullamcorper nulla leo, at tristique enim pellentesque eget. Proin vel interdum odio. Nullam malesuada odio vitae viverra blandit. Mauris massa dolor, finibus vel semper et, dapibus a sapien. Suspendisse est augue, mattis in sodales quis, blandit et erat. Curabitur ac maximus lacus.")
             }
             .navigationTitle("🍊 План питания")
-            .padding(.top)
+            .padding()
+            .scrollBounceBehavior(.basedOnSize)
         }
     }
 }
 
-//                Text(viewModel.messages.filter( {$0.role == .assistant } ).last?.content ?? "Sample text")
 
 #Preview {
     CalendarView(viewModel: ViewModel())
