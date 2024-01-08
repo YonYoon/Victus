825a5322c58d75cb97224d357ef1e05b01910b1a
YonYoon <zhzhzh1806@gmail.com>
2024-01-08 22:06:24 +0500

    Wrapped view in NavigationStack

--- Victus/Views/ChatView.swift
+++ Victus/Views/ChatView.swift
@@ -10,41 +10,57 @@ import SwiftUI
 struct ChatView: View {
     @ObservedObject var viewModel = ViewModel()
     
+    @FocusState private var isTextFieldFocused: Bool
+    
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "person.circle.fill")
-                    .imageScale(.large)
-                    .font(.title)
-                    .foregroundStyle(.accent)
-                    .symbolRenderingMode(.multicolor)
-                Text("Диетолог")
-                    .font(.title2)
-                    .fontWeight(.medium)
-            }
-            
-            ScrollView {
-                ForEach(viewModel.messages.filter({$0.role != .system}), id:\.id) { message in
-                    messageView(message: message)
-                }
-            }
-            
-            HStack {
-                TextField("Спросите о диете...", text: $viewModel.currentInput)
-                    .padding()
-                    .background(.gray.opacity(0.2))
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-                Button {
-                    viewModel.sendMessage()
-                } label: {
-                    Image(systemName: "paperplane.fill")
+        NavigationStack {
+            VStack {
+                HStack {
+                    Image(systemName: "person.circle.fill")
                         .imageScale(.large)
+                        .font(.title)
+                        .foregroundStyle(.accent)
+                        .symbolRenderingMode(.multicolor)
+                    Text("Диетолог")
                         .font(.title2)
+                        .fontWeight(.medium)
                 }
-                .tint(.accent)
+                
+                ScrollView {
+                    ForEach(viewModel.chatMessages.filter({$0.role != .system}), id:\.id) { message in
+                        messageView(message: message)
+                    }
+                }
+                
+                HStack {
+                    TextField("Спросите о диете...", text: $viewModel.currentInput)
+                        .padding()
+                        .background(.gray.opacity(0.2))
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .focused($isTextFieldFocused)
+                    Button {
+                        if !viewModel.currentInput.isEmpty {
+                            viewModel.sendChatMessage()
+                        }
+                    } label: {
+                        Image(systemName: "paperplane.fill")
+                            .imageScale(.large)
+                            .font(.title2)
+                    }
+                    .tint(.accent)
+                }
+            }
+            .padding()
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Готово") {
+                        isTextFieldFocused = false
+                    }
+                }
             }
         }
-        .padding()
     }
     
     func messageView(message: Message) -> some View {
